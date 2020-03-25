168c559b2a1b781e1334ff3f02e955b0c630fe42
Brian Heise <bnheise@gmail.com>
2020-03-25 18:16:56 +0900

    added google map for react

--- src/containers/Map.jsx
+++ src/containers/Map.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 export default class Map extends Component {
   render() {
+    let marker = null;
+    let center = { lat: 48.856614, lng: 2.352222 }
     const style = {
       height: "100vh"
     };
 
     return (
       <div className="col-sm-5" style={style}>
-        TODO
+        <GoogleMapReact
+          center={center}
+          defaultZoom={15}
+        >
+          {marker}
+        </GoogleMapReact>
       </div>
     );
   }
