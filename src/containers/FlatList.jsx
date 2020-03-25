5337ca6f654a774d32122b06d73c0f858c8a069f
Brian Heise <bnheise@gmail.com>
2020-03-25 14:40:29 +0900

    fixed typo in import line

--- src/containers/FlatList.jsx
+++ src/containers/FlatList.jsx
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 export default class FlatList extends Component {
   render() {
     return (
-      <div className="col-7">
+      <div className="col-sm-7">
         TODO
       </div>
     )
