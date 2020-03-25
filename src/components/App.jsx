2064f033e24eade050fa044c479903eac1f1f73a
Brian Heise <bnheise@gmail.com>
2020-03-25 14:33:34 +0900

    set up basic react app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import FlatList from '../containers/FlatList';
+import Map from '../containers/Map';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <FlatList />
+        <Map />
       </div>
     )
   }
---
+++ src/containers/FlatList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class FlatList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
